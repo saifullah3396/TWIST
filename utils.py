8ed90fb4bbf40281d2988dbe9d2941bcfb13aa9d
saifullah3396 <saifullah3396@gmail.com>
2021-11-17 14:59:48 +0100

    Testing...

--- utils.py
+++ utils.py
@@ -229,6 +229,7 @@ def save_on_master(*args, **kwargs):
 
 
 def init_distributed_mode(args):
+    print(os.environ['SLURM_PROCID'])
     if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
         args.rank = int(os.environ["RANK"])
         args.world_size = int(os.environ['WORLD_SIZE'])
