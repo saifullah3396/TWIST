d5a13df2e9966fa8488461e6d3a4cabb9905cf6e
saifullah3396 <saifullah3396@gmail.com>
2021-11-17 15:29:47 +0100

    Teting..

--- utils.py
+++ utils.py
@@ -245,14 +245,14 @@ def init_distributed_mode(args):
 
     args.distributed = True
     
-    print('args.gpu', args.gpu, args.rank, args.world_size)
+    print('args.gpu', args.gpu, args.rank, args.world_size, torch.cuda.device_count())
     torch.cuda.set_device(args.gpu)
     args.dist_backend = 'nccl'
     print('| distributed init (rank {}): {}'.format(
         args.rank, args.dist_url), flush=True)
     torch.distributed.init_process_group(backend=args.dist_backend, init_method=args.dist_url, rank=args.rank, world_size=args.world_size)
-    # torch.distributed.barrier()
-    # setup_for_distributed(args.rank == 0)
+    torch.distributed.barrier()
+    setup_for_distributed(args.rank == 0)
 
 @torch.no_grad()
 def concat_all_gather(tensor):
