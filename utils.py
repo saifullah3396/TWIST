682cb9662d7a6636dce2e9d43e1da03006257aa8
saifullah3396 <saifullah3396@gmail.com>
2021-11-17 15:01:44 +0100

    Testing...

--- utils.py
+++ utils.py
@@ -229,8 +229,6 @@ def save_on_master(*args, **kwargs):
 
 
 def init_distributed_mode(args):
-    os.environ['MASTER_ADDR'] = os.environ['SLURM_LAUNCH_NODE_IPADDR']
-   
     if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
         args.rank = int(os.environ["RANK"])
         args.world_size = int(os.environ['WORLD_SIZE'])
