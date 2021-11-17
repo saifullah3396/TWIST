519444ddf7ea2b2e86319a80c81189147fa8fdcf
saifullah3396 <saifullah3396@gmail.com>
2021-11-17 15:17:04 +0100

    Updated.

--- utils.py
+++ utils.py
@@ -235,7 +235,7 @@ def init_distributed_mode(args):
         args.gpu = int(os.environ['LOCAL_RANK'])
     elif 'SLURM_PROCID' in os.environ:
         os.environ['MASTER_ADDR'] = os.environ['SLURM_LAUNCH_NODE_IPADDR']
-        args.world_size = os.environ['WORLD_SIZE']
+        args.world_size = int(os.environ['WORLD_SIZE'])
         args.rank = int(os.environ['SLURM_PROCID'])
         args.gpu = args.rank % torch.cuda.device_count()
     else:
