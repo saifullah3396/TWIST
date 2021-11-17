553fb4a8f2e62c7f898bc44d18b556c53f913a4a
saifullah3396 <saifullah3396@gmail.com>
2021-11-17 15:40:51 +0100

    Test.

--- utils.py
+++ utils.py
@@ -249,7 +249,7 @@ def init_distributed_mode(args):
     
     print('args.gpu', args.gpu, args.rank, args.world_size, torch.cuda.device_count())
     torch.cuda.set_device(args.gpu)
-    args.dist_backend = 'nccl'
+    args.dist_backend = 'NCCL'
     print('| distributed init (rank {}): {}'.format(
         args.rank, args.dist_url), flush=True)
     torch.distributed.init_process_group(backend=args.dist_backend, init_method=args.dist_url, rank=args.rank, world_size=args.world_size)
